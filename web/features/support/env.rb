7e5427af5ad02e0e64747d3b553aac7d5b65947a
joaolucasf <joaolucasfernandes@gmail.com>
2021-11-12 23:49:59 -0300

    Fix chrome headless and add webdrivers gem

--- web/features/support/env.rb
+++ web/features/support/env.rb
@@ -2,6 +2,7 @@ require "allure-cucumber"
 require "capybara"
 require "capybara/cucumber"
 require "faker"
+require "webdrivers"
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["CONFIG"]}"))
 
@@ -15,18 +16,18 @@ when "fire_headless"
 when "chrome_headless"
   Capybara.register_driver :selenium_chrome_headless do |app|
     version = Capybara::Selenium::Driver.load_selenium
-    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
+    
     browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
       opts.add_argument("--headless")
-      opts.add_argument("--disable-gpu")
+      opts.add_argument("--disable-gpu") if Gem.win_platform?
+      # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
       opts.add_argument("--disable-site-isolation-trials")
       opts.add_argument("--no-sandbox")
       opts.add_argument("--disable-dev-shm-usage")
     end
 
-    Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
+    Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options: browser_options })
   end
-
   @driver = :selenium_chrome_headless
 else
   raise "Invalid Browser :("
